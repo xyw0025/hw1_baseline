cfa69728aea285a62d647d1a1eb38cc2de4f2395
Anshumaan-Chauhan02 <anshjan4@gmail.com>
2023-09-24 13:06:42 -0400

    Function descriptions added in Transaction class - set and get methods for variables

--- expense_tracker/src/Transaction.java
+++ expense_tracker/src/Transaction.java
@@ -21,27 +21,34 @@ public class Transaction {
     this.timestamp = generateTimestamp();
   }
 
+  //Function to access value of the amount variable
   public double getAmount() {
     return amount;
   }
 
+  // Function to set a value to the amount variable
   public void setAmount(double amount) {
     this.amount = amount;
   }
 
+  // Function to access the value of category variable
   public String getCategory() {
     return category;
   }
 
+  // function to assign/set a value to the category variable
   public void setCategory(String category) {
     this.category = category; 
   }
   
+  // function to return the timestamp value
   public String getTimestamp() {
     return timestamp;
   }
 
+  // Generates and returns the current timestamp in the pattern "dd-MM-yyyy HH:mm"
   private String generateTimestamp() {
+    // Formatter to load the timestamp according to a specific pattern
     SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");  
     return sdf.format(new Date());
   }
